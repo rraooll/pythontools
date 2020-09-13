7829e1a71ac465df139e3f63d16e63080465e5c5
Rahul <60598818+rraooll@users.noreply.github.com>
2020-09-13 09:57:10 +0530

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -1,19 +1,32 @@
 import socket
+import threading
 
 # selecting host and port
 target_host = 'Domain.name'
 target_port = 443
 # creating socket
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 #connecting our socket to targeted host an domain
-s.connect((target_host,target_port))
+server.bind((target_host,target_port))
 
-#sending data
-s.send('GET/HTTP/1.1\r\nHost:domain.com\r\n\r\n')
+server.listen()
 
-#receving response
-response = s.recv(4096)
+print("[*] Listening on %s:%d" %(target_host,target_port))
 
-#printing response
-print(response)
+#handling requests
+def handle_client(client_socket):
+  request = client_socket.recv(1024)
+  print("[*] Recevied: {}".format(request))
+  
+  client_socket.send("OHH")
+  
+  client_socket.close()
+while True:
+  client,addr = server.accept()
+  print("[*] Accepted connection from : {}:{}".format(addr[0],addr[1]))
+  
+  client_handler = threading.Thread(target=handle_client,args=(client,))
+  
+  client_handler.start()
+  
